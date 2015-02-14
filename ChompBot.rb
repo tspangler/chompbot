419981f4b2d19c0144fe856c7ee3ae5e0b54f8d8
Timothy Spangler <tim@timspangler.com>
2015-02-14 17:21:18 -0600

    Don't correct users within two hours of their last correction.
    Ignore retweets.

--- ChompBot.rb
+++ ChompBot.rb
@@ -3,6 +3,8 @@
 require 'rubygems'
 require 'chatterbot/dsl'
 require 'colorize'
+require 'active_support'
+require 'active_support/core_ext'
 
 # debug_mode
 verbose
@@ -17,47 +19,67 @@ puts "Starting #{'ChompBot'.colorize(:light_cyan)} using the streaming API..."
 
 streaming do
   search "chomping at the bit" do |tweet|
-    responses = [
-      '#USER# The correct term is "champing."',
-      '#USER# I believe you meant "champing."',
-      '#USER# Did you know that the term is actually "champing at the bit?"',
-      '#USER# I think you meant to say "champing at the bit."',
-      '#USER# FYI, I think you wanted "champing" at the bit.'
-    ]
+    # Have we corrected this user in the last two hours?
+    if (tweet.created_at < 2.hours.ago) && !tweet.retweet?
+      responses = [
+        '#USER# The correct term is "champing."',
+        '#USER# I believe you meant "champing."',
+        '#USER# Did you know that the term is actually "champing at the bit?"',
+        '#USER# I think you meant to say "champing at the bit."',
+        '#USER# FYI, I think you wanted "champing" at the bit.',
+        '#USER# It\'s actually "champing," not "chomping."'
+      ]
 
-    # Fave everybody we correct
-    favorite tweet
+      # Have we corrected this user in the last hour?
+      this_user = tweet.user.id
 
-    # Pick a response at random and append the link
-    reply responses.sample + ' http://chompbot.herokuapp.com', tweet
-
-    # Log it
-    data = {}
-    
-    data['tweet_id'] = tweet.id
-    data['text'] = tweet.text
-    data['from_user'] = tweet.user.username
-    data['from_user_id'] = tweet.user.id
-    data['created_at'] = tweet.created_at
-    
-    # Serialize the entire tweet for the hell of it
-    data['tweet_json'] = tweet.to_h.to_json
-
-    # Look for coordinates; add if we have them
-    if tweet.geo.coordinates
-      data['lat'] = tweet.geo.coordinates[0].nil? ? '' : tweet.geo.coordinates[0] 
-      data['lng'] = tweet.geo.coordinates[1].nil? ? '' : tweet.geo.coordinates[1]
-    end
-
-    # Add a city if we have one
-    if tweet.place
-      if tweet.place.place_type == 'city'
-        data['city'] = tweet.place.full_name.nil? ? '' : tweet.place.full_name
+      # Fave everybody we correct
+      begin
+        favorite tweet
+      rescue Exception => e
+        puts "{'Could not favorite tweet.'.colorize(:red)}"
+        puts 'Exception:'.colorize(:red) + ' ' + e.message
       end
-      
-      data['country'] = tweet.place.country_code.nil? ? '' : tweet.place.country_code
-    end    
 
-    db[:chompbot].insert(data)
+
+      # Pick a response at random and append the link
+      begin
+        reply responses.sample + ' http://chompbot.herokuapp.com', tweet
+      rescue Exception => e
+        puts "{'Could not reply to tweet.'.colorize(:red)}"
+        puts 'Exception:'.colorize(:red) + ' ' + e.message
+      end
+
+      # Log it
+      data = {}
+    
+      data['tweet_id'] = tweet.id
+      data['text'] = tweet.text
+      data['from_user'] = tweet.user.username
+      data['from_user_id'] = tweet.user.id
+      data['created_at'] = tweet.created_at
+    
+      # Serialize the entire tweet for the hell of it
+      data['tweet_json'] = tweet.to_h.to_json
+
+      # Look for coordinates; add if we have them
+      if tweet.geo.coordinates
+        data['lat'] = tweet.geo.coordinates[0].nil? ? '' : tweet.geo.coordinates[0] 
+        data['lng'] = tweet.geo.coordinates[1].nil? ? '' : tweet.geo.coordinates[1]
+      end
+
+      # Add a city if we have one
+      if tweet.place
+        if tweet.place.place_type == 'city'
+          data['city'] = tweet.place.full_name.nil? ? '' : tweet.place.full_name
+        end
+      
+        data['country'] = tweet.place.country_code.nil? ? '' : tweet.place.country_code
+      end    
+
+      db[:chompbot].insert(data)
+    else
+      puts 'Not correcting user: too soon since last correction or tweet is a retweet'
+    end    
   end
 end
